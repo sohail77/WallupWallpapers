0e460ce0d3bec32a94375be36f18acc7ecfbbacd
Mohammed Sohail <sohail778899@gmail.com>
2018-08-15 20:27:21 +0530

    Added splash screen

---
+++ app/src/main/java/com/sohail/wallupwallpapers/Activities/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.sohail.wallupwallpapers.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sohail.wallupwallpapers.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
